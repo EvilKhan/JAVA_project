8912ce27750890902723d8402f80d807e85c3ba0
UsalTatar <barbariandiii@gmail.com>
2023-03-25 21:04:07 +0700

    task2 done

---
+++ homework1_2.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Вывести все простые числа от 1 до 1000
+public class homework1_2 {
+  public static void main(String[] args) {
+    int nums, count;
+    for (nums = 1; nums <1001; nums++) {
+        count = 0;
+        for (int i = 1; i <= nums; i++) {
+            if (nums % i == 0)
+                count++;
+        }
+        if (count <= 2)
+            System.out.printf("%d,", nums);
+
+    }
+}
+}
